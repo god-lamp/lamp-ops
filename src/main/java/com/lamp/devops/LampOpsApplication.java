f10e05763a00e6fbaccd2429ce5b85622c3e2b4c
18180556172 <liuzhangyu1@sctel.com.cn>
2024-02-20 18:00:53 +0800

    modify:修改主类名称

--- src/main/java/com/lamp/devops/LampOpsApplication.java
+++ src/main/java/com/lamp/devops/LampOpsApplication.java
@@ -2,12 +2,16 @@ package com.lamp.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
+@EnableAsync
+@EnableWebSocket
+@EnableScheduling
 @SpringBootApplication
 public class LampOpsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LampOpsApplication.class, args);
     }
-
 }
