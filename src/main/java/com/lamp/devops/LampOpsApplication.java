146c94c49678ec7076114576d9cef2174f7dfeab
god-lamp <2545977650@qq.com>
2024-02-20 22:36:29 +0800

    feat:启动异步执行和定时执行功能

--- src/main/java/com/lamp/devops/LampOpsApplication.java
+++ src/main/java/com/lamp/devops/LampOpsApplication.java
@@ -2,12 +2,14 @@ package com.lamp.devops;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableAsync
+@EnableScheduling
 @SpringBootApplication
 public class LampOpsApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LampOpsApplication.class, args);
     }
-
 }
